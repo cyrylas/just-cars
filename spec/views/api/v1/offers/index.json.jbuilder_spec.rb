ce37a113446c7f26f31d7fe130a02d0ee73797e4
Michał Wazgird <cyryl@cyryl.net>
2019-11-03 15:54:57 +0100

    Sometimes float check failed.
    It compared: `0.575172e4 != 5751.72`
    
    Make sure, that we would be checking same values

--- spec/views/api/v1/offers/index.json.jbuilder_spec.rb
+++ spec/views/api/v1/offers/index.json.jbuilder_spec.rb
@@ -11,11 +11,18 @@ RSpec.describe 'api/v1/offers/index.json.jbuilder', type: :view do
     expect(JSON.parse(rendered).size).to eq(2)
   end
 
-  it 'should contain title, description and price' do
+  it 'should contain title, description' do
     render
     resp = JSON.parse(rendered)
     %w[title description price].each do |attr|
       expect(resp.pluck(attr)).to eq(offers.pluck(attr))
     end
+  end
+  it 'should contain  price' do
+    render
+    resp = JSON.parse(rendered)
+    # Sometimes price was converted to float as 0.575172e4
+    # Make sure, that both values would be same
+    expect(resp.pluck('price').map(&:to_s)).to eq(offers.pluck('price').map(&:to_s))
   end
 end
