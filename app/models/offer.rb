ae03631e29b06458c535cd643c6fe5436286fe8a
Michał Wazgird <cyryl@cyryl.net>
2019-11-03 21:32:42 +0100

    validate content type of incoming files

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,4 +6,5 @@ class Offer < ApplicationRecord
   validates :title, length: { minimum: 3, maximum: 100 }
   validates :description, length: { maximum: 3_000 }
   validates :price, numericality: { greater_than_or_equal_to: 0.01, less_than: 1_000_000.00 }
+  validates :picture, content_type: /\Aimage\/.*\z/
 end
